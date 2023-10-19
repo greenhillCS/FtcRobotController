aa9a94797f91f809ab23298c87c4eafc0ececc93
VedantSubramanian <143726508+VedantSubramanian@users.noreply.github.com>
2023-10-19 09:29:23 -0500

    Update GreenhillHardware.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensors/GreenhillHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensors/GreenhillHardware.java
@@ -5,5 +5,9 @@ public class GreenhillHardware {
     protected static final String TOUCH_SENSOR = "touchSensor";
     protected static final String LEFT_SERVO = "LServo";
     protected static final String RIGHT_SERVO = "RServo";
+    protected static final String BACK_RIGHT_MOTOR = "BR";
+    protected static final String BACK_LEFT_MOTOR = "BL";
+    protected static final String FRONT_RIGHT_MOTOR = "FR";
+    protected static final String FRONT_LEFT_MOTOR = "FL";
 
 }
